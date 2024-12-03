a43718f511707c9b2618e2ba0a695df69badbd66
DanielKremes <danielkremes06@gmail.com>
2024-12-02 22:26:09 -0300

    add folder "Store and retrieve data using literal and variable values in C#" and set configurations for run code

--- Write your first C# code/Program.cs
+++ Write your first C# code/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("i wrote my first line in code C#");
 
+var text = "1234";
+var is_active = false;
+var decunal = 10.30m;
+var double_point = 10f;
+
+Console.WriteLine("" + text);
+Console.WriteLine("" + is_active);
+Console.WriteLine("" + decunal);
+Console.WriteLine("" + double_point);
